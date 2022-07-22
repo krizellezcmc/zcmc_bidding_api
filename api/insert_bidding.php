99506ba555c8e582d039a186be04d8b5c060818b
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-07-22 13:59:29 +0800

    krizelle 1:59

--- api/insert_bidding.php
+++ api/insert_bidding.php
@@ -17,29 +17,46 @@ $method = $_SERVER['REQUEST_METHOD'];
             $qty = $details->quantity;
 
             $supplierDetails = $details->suppliers;
-        
-            $stmt = $db->prepare("INSERT INTO item(PK_itemId, itemDesc, quantity) VALUES(?, ?, ?)");
-            $stmt->bind_param("isd", $itemId, $item, $qty);
 
-            if($stmt->execute()) {
+            $object = array_reduce($supplierDetails, function($a, $b){
+                return $a->unitCost < $b->unitCost ? $a : $b;
+            }, array_shift($supplierDetails));
 
-                foreach($supplierDetails as $key => $val){
-                    $supplierId = $val->supplierId;
-                    $unitCost = $val->unitCost;
+            $wSupplierId = $object->supplierId;
+            $wUnitCost = $object->unitCost;
 
-                    $bidding = $db->prepare("INSERT INTO bidding(FK_itemId, FK_supplierId, unitCost) VALUES(?, ?, ?)");
-                    $bidding->bind_param("iid", $itemId, $supplierId, $unitCost);
+            $winner = $db->prepare("INSERT INTO winners(wItemDesc, wQuantity, FK_supplierId, wCost) VALUES(?, ?, ?, ?)");
+            $winner->bind_param("siid", $item, $qty, $wSupplierId, $wUnitCost);
 
-                    if($bidding->execute()){
-                        $data = ['status' => 1, 'message' => "Success bidding entry"];
-                    }else {
-                        $data = ['status' => 0, 'message' => "Failed bidding entry"];
+            if($winner->execute()) {
+
+                $stmt = $db->prepare("INSERT INTO item(PK_itemId, itemDesc, quantity) VALUES(?, ?, ?)");
+                $stmt->bind_param("isd", $itemId, $item, $qty);
+
+                if($stmt->execute()) {
+
+                    foreach($supplierDetails as $key => $val){
+                        $supplierId = $val->supplierId;
+                        $unitCost = $val->unitCost;
+
+
+                        $bidding = $db->prepare("INSERT INTO bidding(FK_itemId, FK_supplierId, unitCost) VALUES(?, ?, ?)");
+                        $bidding->bind_param("iid", $itemId, $supplierId, $unitCost);
+
+                        if($bidding->execute()){
+                            $data = ['status' => 1, 'message' => "Success bidding entry"];
+                        }else {
+                            $data = ['status' => 0, 'message' => "Failed bidding entry"];
+                        }
+
                     }
+                    
 
+                } else {
+                    $data = ['status' => 0, 'message' => "Failed"];
                 }
-
             } else {
-                $data = ['status' => 0, 'message' => "Failed"];
+                 $data = ['status' => 0, 'message' => "Failed winner insert"];
             }
 
             echo json_encode($data);
