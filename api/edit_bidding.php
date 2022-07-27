5ee92e109f6526eddec276712275bd3059018073
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-07-27 14:52:28 +0800

    kriz

---
+++ api/edit_bidding.php
@@ -0,0 +1,42 @@
+<?php
+
+include '../connection/config.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+    switch($method) {
+        case 'POST':
+            $details = json_decode(file_get_contents('php://input'));
+
+            $itemId = $details->itemId;
+            $item = $details->itemDesc; 
+            $qty = $details->quantity;
+            $supplierDetails = $details->suppliers;
+
+                $stmt = $db->prepare("UPDATE item SET itemDesc = ?, quantity = ?, lastEdit = CURRENT_TIMESTAMP() WHERE PK_itemId = ?");
+                $stmt->bind_param("sdi", $item, $qty, $itemId);
+
+                if($stmt->execute()) {
+                    foreach($supplierDetails as $key => $val){
+                        $supplierId = $val->supplierId;
+                        $unitCost = $val->unitCost;
+
+                        $bidding = $db->prepare("UPDATE bidding SET FK_supplierId = ?, unitCost = ? WHERE FK_supplierId = ?");
+                        $bidding->bind_param("idi", $supplierId, $unitCost, $supplierId);
+
+                        if($bidding->execute()){
+                            $data = ['status' => 1, 'message' => "Success edit"];
+                        }else {
+                            $data = ['status' => 0, 'message' => "Edit failed"];
+                        }
+                    }
+
+                } else {
+                    $data = ['status' => 0, 'message' => "Failed"];
+                }
+            
+            echo json_encode($data);
+            break;
+}
+
+?>
--- api/get_bidding_data.php
+++ api/get_bidding_data.php
@@ -7,24 +7,21 @@ $method = $_SERVER['REQUEST_METHOD'];
     switch($method) {
         case 'GET':
 
-            $item = $db->prepare('SELECT PK_itemId, itemDesc, quantity FROM item where PK_itemId = 977397');
-            // $row->bind_param('s', $_GET['dateSelected']);
+            $item = $db->prepare('SELECT PK_itemId, itemDesc, quantity, lastEdit FROM item where PK_itemId = ? AND dateAdded = ?' );
+            $item->bind_param('is', $_GET['itemId'], $_GET['dateSelected']);
             $item->execute();
             $get = $item->get_result();
-            $item = $get->fetch_all(MYSQLI_ASSOC);
+            $item = $get->fetch_assoc();
 
-            $stmt = $db->prepare('SELECT bidding.FK_supplierId, supplier.supplierName, bidding.unitCost from bidding INNER JOIN item ON bidding.FK_itemId = item.PK_itemId INNER JOIN supplier ON bidding.FK_supplierId = supplier.PK_supplierId where bidding.FK_itemId = 977397 GROUP BY bidding.FK_supplierId');
-            // $row->bind_param('s', $_GET['dateSelected']);
+            $stmt = $db->prepare('SELECT bidding.FK_supplierId as supplierId, bidding.unitCost as unitCost, bidding.lastEdit as lastEdit from bidding INNER JOIN item ON bidding.FK_itemId = item.PK_itemId INNER JOIN supplier ON bidding.FK_supplierId = supplier.PK_supplierId WHERE bidding.FK_itemId = ? AND bidding.dateAdded = ? GROUP BY bidding.FK_supplierId');
+            $stmt->bind_param('is', $_GET['itemId'], $_GET['dateSelected']);
             $stmt->execute();
-            $get = $stmt->get_result();
+            $get = $stmt->get_result(); 
             $result = $get->fetch_all(MYSQLI_ASSOC);
-
 
             $storage = array_push($item, $result);
 
            echo json_encode($item);
-
            
-
         break;
     }
